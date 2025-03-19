aae8088ca156e3fa043a951fb57ad85da04fd331
abeaar <abraarjh@gmail.com>
2025-03-19 23:25:15 +0700

    apalaha

--- index.js
+++ index.js
@@ -12,4 +12,4 @@ app.get("/", (req, res) => {
 });
 
 
-app.listen(5000, () => console.log("Server is running on port 5000"));
+app.listen(3002, () => console.log("Server is running on port 5000"));
