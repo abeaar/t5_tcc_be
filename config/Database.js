853d37a5b56bf2e5ec94624ad637c18ae60140d6
abeaar <140606965+abeaar@users.noreply.github.com>
2025-03-19 22:32:21 +0700

    Update Database.js

--- config/Database.js
+++ config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize';
 
-const dbContext = new Sequelize("t3_notes_109", "root", "", {
+const dbContext = new Sequelize("t3_notes_109", "root", "KorupsiTimah$300T", {
   host: "34.70.179.84",
   dialect: "mysql",
 }); // Database name
